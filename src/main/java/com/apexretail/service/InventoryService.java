bcb21ac0a3045602282e63fa28f9d082c417f716
D6uez <dpantech@gmail.com>
2025-12-15 23:23:01 -0500

    Added package for service layer, started a skeleton for InventoryService

---
+++ src/main/java/com/apexretail/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.apexretail.service;
+
+import com.apexretail.domain.Product;
+
+public class InventoryService {
+
+    public void sellProduct(Product prod, int amount) {
+        validateProduct(prod);
+
+    }
+
+    public void restockProduct(Product prod, int amount) {
+
+    }
+
+    private void validateProduct(Product prod) {
+
+    }
+
+}
